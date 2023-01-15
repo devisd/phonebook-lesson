5d2355a09dc2e31fb2b19c0557515cb17f62975d
Devis Dumler <devisdumler@gmail.com>
2023-01-15 17:12:55 +0700

    adding gh-pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { store } from 'redux/store';
 import App from './components/App';
 
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <BrowserRouter> */}
-      <App />
-      {/* </BrowserRouter> */}
+      <BrowserRouter basename="phonebook-lesson">
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
